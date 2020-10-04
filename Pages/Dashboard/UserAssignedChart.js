70e5854738f67822ac4bf029098ab365b786e44f
Ziah-M <65767717+Ziah-M@users.noreply.github.com>
2020-10-05 07:49:14 +1100

    Add user ticket load chart

--- Pages/Dashboard/UserAssignedChart.js
+++ Pages/Dashboard/UserAssignedChart.js
@@ -3,26 +3,29 @@ import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 import { convertObjectToList } from '../../Helpers'
 import ChartReact from '../../../../lib/charts/charts.react'
+import { faCcVisa } from '@fortawesome/fontawesome-free-brands'
 
 let { Chart } = ChartReact
 
 const PriorityChart = () => {
   const tickets = useSelector((state) => state.tickets)
-  const none = convertObjectToList(tickets).filter(
-    (ticket) => ticket.priority.toLowerCase() === 'none',
-  )
+  const ticketsArray = convertObjectToList(tickets)
 
-  const low = convertObjectToList(tickets).filter(
-    (ticket) => ticket.priority.toLowerCase() === 'low',
-  )
+  // Assigns integer of number of tickets for each developer assigned to at least 1 ticket
+  let userTickets = {}
 
-  const medium = convertObjectToList(tickets).filter(
-    (ticket) => ticket.priority.toLowerCase() === 'medium',
-  )
+  ticketsArray.map((ticket) => {
+    userTickets[ticket.assignedTo] = !userTickets[ticket.assignedTo]
+      ? 1
+      : userTickets[ticket.assignedTo] + 1
+  })
 
-  const high = convertObjectToList(tickets).filter(
-    (ticket) => ticket.priority.toLowerCase() === 'high',
-  )
+  const dataPoints = Object.keys(userTickets).map((userName) => {
+    return {
+      name: userName,
+      y: userTickets[userName],
+    }
+  })
 
   const chartEl = useRef(null)
   const options = {
@@ -41,15 +44,12 @@ const PriorityChart = () => {
     data: [
       {
         type: 'doughnut',
-        dataPoints: [
-          { name: 'None', y: none.length || 0 },
-          { name: 'Low', y: low.length || 0 },
-          { name: 'Medium', y: medium.length || 0 },
-          { nam: 'High', y: high.length || 0 },
-        ],
+        showInLegend: true,
+        dataPoints: dataPoints,
       },
     ],
   }
+
   return (
     <Wrapper>
       <Chart options={options} ref={chartEl} />
